fa8ced50c898515435812faede4e9a73752920a6
Saymon Gabriel <saymonzinho117@gmail.com>
2026-02-11 20:58:00 -0300

    Atividade modelo fisico

---
+++ AtividadeMF/questao05.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Pedido
+(
+id_pedido SERIAL primary key,
+dia DATE,
+VALOR float
+);
+
+CREATE TABLE Produto
+(
+id_produto SERIAL primary key,
+DESCRICAO VARCHAR(100),
+valor FLOAT
+);
+
+
+CREATE TABLE Cliente
+(
+id_cliente SERIAL primary key,
+CPF VARCHAR(14),
+Nome VARCHAR(50),
+pedido_idfk int,
+FOREIGN KEY (pedido_idfk) REFERENCES pedido(id_pedido)
+);
+
+
+CREATE TABLE Venda
+(
+id_venda SERIAL PRIMARY KEY,
+Quantidade int,
+pedido_idfk int,
+produto_idfk int,
+FOREIGN KEY  (pedido_idfk) REFERENCES pedido(id_pedido),
+FOREIGN KEY (produto_idfk) REFERENCES produto(id_produto)
+);
